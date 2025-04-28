1e24cbfec455b19bbcbfefec54f2c3984c232b84
Paul <pauldecalf@outlook.fr>
2025-04-28 13:28:37 +0200

    edit

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 # model.py
 
-"""Model module for sentiment prediction. .""" 
+"""Model module for sentiment prediction . .""" 
 def predict_sentiment(text):
     """Predict the sentiment of a given text as positive, negative, or neutral."""
     if not text:
